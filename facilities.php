056a453ee47858fa0a3b03300bc3cb1cf891c35b
Kabir Ahmed Ridoy <kabir.cse.bd@gmail.com>
2025-06-10 01:31:30 +0600

    about page done

--- facilities.php
+++ facilities.php
@@ -5,10 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Our Hotel - Facilities</title>
-    <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
     <?php require('inc/links.php'); ?>
+
     <style>
         .pop:hover {
             border-top-color: var(--teal) !important;
